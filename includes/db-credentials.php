5f2aaca40fe782043e9e23f05811ba48cf6bc4d3
Iyd4raf <43270055+Iyd4raf@users.noreply.github.com>
2019-05-01 20:14:03 +0100

    Update db-credentials.php

--- includes/db-credentials.php
+++ includes/db-credentials.php
@@ -1,8 +1,8 @@
 <?php
 
 define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "admin");
+define("DB_USER", "user");
+define("DB_PASS", "password");
 define("DB_NAME", "basic_php_blog");
 
 ?>
